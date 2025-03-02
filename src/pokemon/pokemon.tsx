7415f2560b05e789bd54e4b08576089f05f8709a
Anthony Ostia <meximanianace@gmail.com>
2025-03-01 21:37:57 -0800

    deat: add animation

--- src/pokemon/pokemon.tsx
+++ src/pokemon/pokemon.tsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react";
 import usePokemon from "../services/usePokemon";
 
 const Pokemon = () => {
@@ -8,10 +9,10 @@ const Pokemon = () => {
       {pokemon?.map((poke) => {
         const name = poke.name[0].toUpperCase() + poke.name.slice(1);
         return (
-          <div className="border rounded-2xl h-40 w-40 flex flex-col items-center justify-center">
+          <motion.div initial={{scale: 1}} whileHover={{scale: 1.1}}  className="border rounded-2xl h-40 w-40 flex flex-col items-center justify-center cursor-pointer">
             <img src={poke.sprites.front_default} alt={poke.name} />
             <p>{name}</p>
-          </div>
+          </motion.div>
         );
       })}
     </div>
