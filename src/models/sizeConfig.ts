2632d2c9109abc1e00db0a6d69660fe642ae3292
Anthony Ostia <meximanianace@gmail.com>
2025-03-09 12:18:19 -0700

    refactor: adjust breakpoints

---
+++ src/models/sizeConfig.ts
@@ -0,0 +1,22 @@
+export const CardSizes = {
+    "xs": {
+        width: "w-[276px]",
+        height: "h-[386px]",
+    },
+    "sm": {
+        width: "sm:w-[150px]",
+        height: "sm:h-[210px]",
+    },
+    "md": {
+        width: "md:w-[200px]",
+        height: "md:h-[280px]",
+    },
+    "lg": {
+        width: "lg:w-[250px]",
+        height: "lg:h-[350px]",
+    },
+    "xl": {
+        width: "xl:w-[300px]",
+        height: "xl:h-[420px]",
+    }
+}
--- src/pokemon/pokemonCard.tsx
+++ src/pokemon/pokemonCard.tsx
@@ -1,8 +1,8 @@
 import { Pokemon } from "../models/pokemon";
 import { FC } from "react";
-import { captialize } from "../utils/converter";
+import { capitalize } from "../utils/converter";
 import { PokemonColors } from "../models/colorConfig";
-
+import { CardSizes } from "../models/sizeConfig";
 
 interface PokemonCardProps {
   pokemon: Pokemon;
@@ -15,44 +15,53 @@ const setPokemonCardBg = (type: string[]) => {
     return {
       cardBg: `bg-gradient-to-t ${firstColor.primaryTo} ${firstColor.secondaryFrom}`,
       imageBg: `bg-gradient-to-t ${firstColor.secondaryTo} ${firstColor.tertiaryFrom}`,
-      textBg: `bg-gradient-to-t ${firstColor.tertiaryTo} ${firstColor.primaryFrom}`
-    }
+      textBg: `bg-gradient-to-t ${firstColor.tertiaryTo} ${firstColor.primaryFrom}`,
+    };
   }
   const secondColor = PokemonColors[type[1]];
 
-  return ({
+  return {
     cardBg: `bg-gradient-to-t ${firstColor.primaryTo} ${secondColor.primaryFrom}`,
     imageBg: `bg-gradient-to-t ${firstColor.secondaryTo} ${secondColor.secondaryFrom}`,
-    textBg: `bg-gradient-to-t ${firstColor.tertiaryTo} ${secondColor.tertiaryFrom}`
-  })
-
-}
+    textBg: `bg-gradient-to-t ${firstColor.tertiaryTo} ${secondColor.tertiaryFrom}`,
+  };
+};
 
 const PokemonCard: FC<PokemonCardProps> = ({ pokemon }) => {
-  const name = captialize(pokemon.name);
-  const colors = setPokemonCardBg(pokemon.pokemon_type)
-  const weight = (pokemon.weight / 10) * 2.20462;
+  const name = capitalize(pokemon.name);
+  const colors = setPokemonCardBg(pokemon.pokemon_type);
 
-  const heightinInches = (pokemon.height / 10) * 39.37008;
-  const feet = Math.floor(heightinInches / 12);
-  const inches = Math.round(heightinInches % 12);
+
+
+  const weight = (pokemon.weight / 10) * 2.20462;
+  const heightInInches = (pokemon.height / 10) * 39.37008;
+  const feet = Math.floor(heightInInches / 12);
+  const inches = Math.round(heightInInches % 12);
   const height = `${feet}'${inches}"`;
   return (
     <div
-      className="rounded-lg h-[350px] w-[250px] p-2 bg-card-background"
+      className={`rounded-lg ${CardSizes["xs"].height} ${CardSizes["xs"].width} p-2 bg-card-background`}
     >
       <div
         className={`flex flex-col p-2 ${colors.cardBg} h-full rounded-lg gap-2`}
       >
         <div className="flex justify-between items-center">
-          <p className="font-medium">{name}</p>
+          <p className="font-medium text-sm">{name}</p>
           <div className="flex gap-2">
-            <p className={`text-white ${PokemonColors[pokemon.pokemon_type[0]].secondaryBg} border rounded-lg p-1`}>
-              {captialize(pokemon.pokemon_type[0])}
+            <p
+              className={`text-white text-sm ${
+                PokemonColors[pokemon.pokemon_type[0]].secondaryBg
+              } border rounded-lg p-1`}
+            >
+              {capitalize(pokemon.pokemon_type[0])}
             </p>
             {pokemon.pokemon_type[1] && (
-              <p className={`text-white ${PokemonColors[pokemon.pokemon_type[1]].secondaryBg} border rounded-lg p-1`}>
-                {captialize(pokemon.pokemon_type[1])}
+              <p
+                className={`text-white text-sm ${
+                  PokemonColors[pokemon.pokemon_type[1]].secondaryBg
+                } border rounded-lg p-1`}
+              >
+                {capitalize(pokemon.pokemon_type[1])}
               </p>
             )}
           </div>
@@ -64,18 +73,21 @@ const PokemonCard: FC<PokemonCardProps> = ({ pokemon }) => {
             <img src={pokemon.image_url} alt={pokemon.name} />
           </div>
           <div
-            className={`flex flex-col justify-center items-center border border-gray-700 rounded-b-lg p-1 bg-card-background gap-1`}
+            className={`flex justify-center items-center border border-gray-700 rounded-b-lg py-1 bg-white gap-1`}
           >
-            <div className="flex gap-2">
-              <p className="text-xs">No. {pokemon.id}</p>
-              <p className="text-xs">{pokemon.species}</p>
+            <div className="flex gap-1">
+              <p className="text-[8px]">No. {pokemon.id}</p>
+              <p className="text-[8px]">{pokemon.species}</p>
             </div>
-            <div className="flex gap-2">
-              <p className="text-xs">HT: {height}</p>
-              <p className="text-xs">WT: {weight.toFixed(1)} lbs</p>
+            <div className="flex gap-1">
+              <p className="text-[8px]">HT: {height}</p>
+              <p className="text-[8px]">WT: {weight.toFixed(1)} lbs</p>
             </div>
           </div>
         </div>
+        <p className="text-xs">
+          {pokemon.flavor_text}
+        </p>
       </div>
     </div>
   );
