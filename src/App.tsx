08a621f6b8493b7a0ce04c166371490de5e196ac
Anthony Ostia <meximanianace@gmail.com>
2025-06-08 15:30:09 -0700

    feat: add clear button

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ const App: FC = () => {
 
   return (
     <div className="flex flex-col w-full relative">
-      <div className="flex flex-col p-8 items-center gap-4 bg-gray-900 min-h-[calc(100vh-112px)]">
+      <div className="flex flex-col p-8 items-center gap-4 bg-gray-900 min-h-[calc(100vh-120px)]">
         <h2 className="text-xl font-bold text-white text-center">
           Welcome to Acedex!
         </h2>
@@ -64,15 +64,24 @@ const App: FC = () => {
           This application will allow you to view every Pokémon in a TCG format.
           Please use the search bar to filter Pokémon.
         </p>
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-2 relative justify-center">
           <input
-            className="bg-white rounded-lg px-2"
+            className="bg-white rounded-lg py-2 pl-2 pr-10 text-gray-800"
             onChange={handleInput}
             type="text"
             aria-label="Search Pokémon"
             placeholder="Search Pokémon..."
             value={input}
           />
+          {input?.length > 0 && (
+            <i
+              className="material-icons absolute right-1 cursor-pointer text-gray-900 hover:bg-gray-100 rounded-full p-1"
+              onClick={() => setInput("")}
+              aria-label="Clear search input"
+            >
+              close
+            </i>
+          )}
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
           {isPokemonFetched &&
@@ -84,9 +93,11 @@ const App: FC = () => {
       </div>
       <button
         onClick={() => window.scroll({ top: 0, behavior: "smooth" })}
-        className={`flex fixed bottom-4 right-4 bg-gray-800 text-white p-2 rounded-full z-10 cursor-pointer hover:bg-gray-700 transition-opacity duration-300 ${isScrollUpVisible ? "opacity-100" : "opacity-0 pointer-events-none"}`}
+        className={`flex fixed bottom-4 right-4 bg-gray-800 text-white p-2 rounded-full z-10 cursor-pointer hover:bg-gray-700 transition-opacity duration-300 ${
+          isScrollUpVisible ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
       >
-        <span className="material-symbols-outlined">arrow_upward</span>
+        <i className="material-icons">arrow_upward</i>
       </button>
       <Footer />
     </div>
